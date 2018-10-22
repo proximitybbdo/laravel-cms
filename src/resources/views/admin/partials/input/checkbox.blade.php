1e44b8847453f65b976da043d49cd3bf5fb74012
Jérémy Dillenbourg <jeremy.dillenbourg@bbdo.bne>
2018-10-22 13:24:27 +0200

    checkbox & radio

---
+++ src/resources/views/admin/partials/input/checkbox.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group form-check">
+    <input type="checkbox" class="form-check-input {{ isset($editor) ? $editor : '' }}" id="{{$field}}" {{ isset($required) ? 'required' : '' }}>
+    <label class="form-check-label" for="{{$field}}">{{$title}}</label>
+</div>
