5b7b16889c2fcc98fb427671014f37a8fa99058e
Jérémy Dillenbourg <jeremy.dillenbourg@bbdo.bne>
2018-10-23 10:10:35 +0200

    refactoring route

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,26 +11,30 @@
 |
 */
 
-Route::group(['prefix' => 'icontrol', 'middleware' => 'web'], function() {
+Route::group(['prefix' => 'icontrol', 'middleware' => 'web','namespace' => '\BBDO\Cms\Http\Controllers\Admin'], function() {
 
-  Route::get('logout', '\BBDO\Cms\Http\Controllers\Admin\SentinelController@logout')->name('logout');
-  Route::get('/', '\BBDO\Cms\Http\Controllers\Admin\SentinelController@showLoginForm')->name('login');
-  Route::get('login', '\BBDO\Cms\Http\Controllers\Admin\SentinelController@showLoginForm')->name('login');
-  Route::post('login', '\BBDO\Cms\Http\Controllers\Admin\SentinelController@login');
+    Route::get('logout', [
+        'uses'  => 'SentinelController@logout',
+        'as'    => 'logout'
+    ]);
+
+  Route::get('/', 'SentinelController@showLoginForm')->name('login');
+  Route::get('login', 'SentinelController@showLoginForm')->name('login');
+  Route::post('login', 'SentinelController@login');
 
   
   Route::group( ['middleware' => \BBDO\Cms\Http\Middleware\Admin\BasicMiddleware::class], function() {
     
-    Route::get('dashboard', '\BBDO\Cms\Http\Controllers\Admin\AdminController@index')->name('dashboard');
-    Route::get('clearcache', '\BBDO\Cms\Http\Controllers\Admin\AdminController@getClearcache')->name('clearcache');
-    Route::post('clearcache', '\BBDO\Cms\Http\Controllers\Admin\AdminController@postClearcache')->name('clearcache');;
-    Route::post('geturlfriendlytext','\BBDO\Cms\Http\Controllers\Admin\HelperController@postUrlFriendlyText')->name('postUrlFriendlyText');;
+    Route::get('dashboard', 'AdminController@index')->name('dashboard');
+    Route::get('clearcache', 'AdminController@getClearcache')->name('clearcache');
+    Route::post('clearcache', 'AdminController@postClearcache')->name('clearcache');;
+    Route::post('geturlfriendlytext','HelperController@postUrlFriendlyText')->name('postUrlFriendlyText');;
 
     Route::group( ['middleware' => \BBDO\Cms\Http\Middleware\Admin\AdminMiddleware::class], function(){
-      Route::get('roles', '\BBDO\Cms\Http\Controllers\Admin\SentinelController@showRolesForm');
-      Route::post('roles', '\BBDO\Cms\Http\Controllers\Admin\SentinelController@assign_roles');
-      Route::get('register', '\BBDO\Cms\Http\Controllers\Admin\SentinelController@showRegistrationForm')->name('register');
-      Route::post('register', '\BBDO\Cms\Http\Controllers\Admin\SentinelController@register');
+      Route::get('roles', 'SentinelController@showRolesForm');
+      Route::post('roles', 'SentinelController@assign_roles');
+      Route::get('register', 'SentinelController@showRegistrationForm')->name('register');
+      Route::post('register', 'SentinelController@register');
     });
 
     Route::group( ['middleware' => \BBDO\Cms\Http\Middleware\Admin\CheckPermissionMiddleware::class], function(){
@@ -38,48 +42,48 @@ Route::group(['prefix' => 'icontrol', 'middleware' => 'web'], function() {
         [
           'prefix' => 'items',
         ], function() {
-        Route::get('{module_type}/overview', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getOverview')->name('overview');
-        Route::get('{module_type}/overview/{link_id}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getOverview')->name('overview');
+        Route::get('{module_type}/overview', 'ItemController@getOverview')->name('overview');
+        Route::get('{module_type}/overview/{link_id}', 'ItemController@getOverview')->name('overview');
 
-        Route::post('{module_type}/overviewdata', '\BBDO\Cms\Http\Controllers\Admin\ItemController@postOverviewData')->name('overviewdata');
-        Route::post('{module_type}/publish', '\BBDO\Cms\Http\Controllers\Admin\ItemController@postPublish')->name('publish');
-        Route::post('{module_type}/featured', '\BBDO\Cms\Http\Controllers\Admin\ItemController@postFeatured')->name('featured');
-        Route::post('{module_type}/delete', '\BBDO\Cms\Http\Controllers\Admin\ItemController@postDelete')->name('delete');
-        Route::post('{module_type}/sortitems', '\BBDO\Cms\Http\Controllers\Admin\ItemController@postSortPost')->name('sort');
-        Route::post('{module_type}/renderblock', '\BBDO\Cms\Http\Controllers\Admin\ItemController@postRenderBlock')->name('render_block');
+        Route::post('{module_type}/overviewdata', 'ItemController@postOverviewData')->name('overviewdata');
+        Route::post('{module_type}/publish', 'ItemController@postPublish')->name('publish');
+        Route::post('{module_type}/featured', 'ItemController@postFeatured')->name('featured');
+        Route::post('{module_type}/delete', 'ItemController@postDelete')->name('delete');
+        Route::post('{module_type}/sortitems', 'ItemController@postSortPost')->name('sort');
+        Route::post('{module_type}/renderblock', 'ItemController@postRenderBlock')->name('render_block');
 
-        Route::get('custom_view/{module_type}/{action}/{lang}/{view_name}/{id}/{back_module_type}/', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItemCustomView');
-        Route::get('custom_view/{module_type}/{action}/{lang}/{view_name}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItemCustomView');
-        Route::get('{module_type}/copylang/{id}/{source_lang}/{destination_lang}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getCopyLangItem')->name('copylang');
-        Route::get('{module_type}/revert/{lang}/{id}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getRevertItem')->name('revert');
-        Route::get('{module_type}/{action}/{lang}/{id}/{back_module_type}/{back_id}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItem');
-        Route::post('{module_type}/{action}/{lang}/{id}/{back_module_type}/{back_id}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItem');
-        Route::get('{module_type}/{action}/{lang}/{id}/{back_module_type}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItem');
-        Route::post('{module_type}/{action}/{lang}/{id}/{back_module_type}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItem');
-        Route::get('{module_type}/{action}/{lang}/{id}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItem')->name('items.edit');
-        Route::get('{module_type}/{action}/{lang}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItem');
-        //Route::post('{module_type}/{action}/{lang}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItem');
+        Route::get('custom_view/{module_type}/{action}/{lang}/{view_name}/{id}/{back_module_type}/', 'ItemController@getAddItemCustomView');
+        Route::get('custom_view/{module_type}/{action}/{lang}/{view_name}', 'ItemController@getAddItemCustomView');
+        Route::get('{module_type}/copylang/{id}/{source_lang}/{destination_lang}', 'ItemController@getCopyLangItem')->name('copylang');
+        Route::get('{module_type}/revert/{lang}/{id}', 'ItemController@getRevertItem')->name('revert');
+        Route::get('{module_type}/{action}/{lang}/{id}/{back_module_type}/{back_id}', 'ItemController@getAddItem');
+        Route::post('{module_type}/{action}/{lang}/{id}/{back_module_type}/{back_id}', 'ItemController@getAddItem');
+        Route::get('{module_type}/{action}/{lang}/{id}/{back_module_type}', 'ItemController@getAddItem');
+        Route::post('{module_type}/{action}/{lang}/{id}/{back_module_type}', 'ItemController@getAddItem');
+        Route::get('{module_type}/{action}/{lang}/{id}', 'ItemController@getAddItem')->name('items.edit');
+        Route::get('{module_type}/{action}/{lang}', 'ItemController@getAddItem');
+        //Route::post('{module_type}/{action}/{lang}', 'ItemController@getAddItem');
 
-        Route::get('{module_type}/{action}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@getAddItem');
-        Route::post('{module_type}/{action}/{lang?}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@store');
-        Route::post('{module_type}/{action}/{lang}/{id}', '\BBDO\Cms\Http\Controllers\Admin\ItemController@store')->name('items.update');
+        Route::get('{module_type}/{action}', 'ItemController@getAddItem');
+        Route::post('{module_type}/{action}/{lang?}', 'ItemController@store');
+        Route::post('{module_type}/{action}/{lang}/{id}', 'ItemController@store')->name('items.update');
       });
       
-      Route::get('files/getimagecontainer/{id}/{type}','\BBDO\Cms\Http\Controllers\Admin\FilesController@getImageContainer');
-      Route::get('files/{manager_type}/manager','\BBDO\Cms\Http\Controllers\Admin\FilesController@getManager');
-      Route::get('files/{manager_type}/manager/{module_type}/{input_id}/{value}', '\BBDO\Cms\Http\Controllers\Admin\FilesController@getManager');
-      Route::get('files/{manager_type}/manager/{module_type}/{input_type}/{input_id}/{value}', '\BBDO\Cms\Http\Controllers\Admin\FilesController@getManager');
-      Route::get('files/{manager_type}/popupmanager/{module_type}/{input_type}/{input_id}', '\BBDO\Cms\Http\Controllers\Admin\FilesController@getPopupManager');
-      Route::get('files/{manager_type}/popupmanager/{module_type}/{input_type}/{input_id}/{value}', '\BBDO\Cms\Http\Controllers\Admin\FilesController@getPopupManager');
-      Route::post('files/{manager_type}/upload', '\BBDO\Cms\Http\Controllers\Admin\FilesController@postUpload');
-      Route::post('files/{manager_type}/{module}/upload', '\BBDO\Cms\Http\Controllers\Admin\FilesController@postUpload');
-      Route::post('files/{manager_type}/{module}/{input_type}/upload', '\BBDO\Cms\Http\Controllers\Admin\FilesController@postUpload');
-      Route::get('files/{manager_type}/getlist/{mode}', '\BBDO\Cms\Http\Controllers\Admin\FilesController@getFiles');
-      Route::get('files/{manager_type}/getlist/{mode}/{input_id}/{module_type}', '\BBDO\Cms\Http\Controllers\Admin\FilesController@getFiles');
-      Route::get('files/{manager_type}/getlist/{mode}/{input_id}/{module_type}/{input_type}', '\BBDO\Cms\Http\Controllers\Admin\FilesController@getFiles');
-      Route::post('files/file_assign_cat', '\BBDO\Cms\Http\Controllers\Admin\FilesController@postAssignCategory');
-      Route::post('files/remove', '\BBDO\Cms\Http\Controllers\Admin\FilesController@postRemove')->name('file_delete');
-      Route::post('files/{manager_type}/purge', '\BBDO\Cms\Http\Controllers\Admin\FilesController@postPurge');
+      Route::get('files/getimagecontainer/{id}/{type}','FilesController@getImageContainer');
+      Route::get('files/{manager_type}/manager','FilesController@getManager');
+      Route::get('files/{manager_type}/manager/{module_type}/{input_id}/{value}', 'FilesController@getManager');
+      Route::get('files/{manager_type}/manager/{module_type}/{input_type}/{input_id}/{value}', 'FilesController@getManager');
+      Route::get('files/{manager_type}/popupmanager/{module_type}/{input_type}/{input_id}', 'FilesController@getPopupManager');
+      Route::get('files/{manager_type}/popupmanager/{module_type}/{input_type}/{input_id}/{value}', 'FilesController@getPopupManager');
+      Route::post('files/{manager_type}/upload', 'FilesController@postUpload');
+      Route::post('files/{manager_type}/{module}/upload', 'FilesController@postUpload');
+      Route::post('files/{manager_type}/{module}/{input_type}/upload', 'FilesController@postUpload');
+      Route::get('files/{manager_type}/getlist/{mode}', 'FilesController@getFiles');
+      Route::get('files/{manager_type}/getlist/{mode}/{input_id}/{module_type}', 'FilesController@getFiles');
+      Route::get('files/{manager_type}/getlist/{mode}/{input_id}/{module_type}/{input_type}', 'FilesController@getFiles');
+      Route::post('files/file_assign_cat', 'FilesController@postAssignCategory');
+      Route::post('files/remove', 'FilesController@postRemove')->name('file_delete');
+      Route::post('files/{manager_type}/purge', 'FilesController@postPurge');
     });
   });
 });
