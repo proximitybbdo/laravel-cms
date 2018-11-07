7894b49833b235222a8b45ccd2c7f7f78417e8ba
Jérémy Dillenbourg <jeremy.dillenbourg@bbdo.bne>
2018-11-07 13:31:47 +0100

    develop translation manager

--- src/resources/views/admin/translation/show.blade.php
+++ src/resources/views/admin/translation/show.blade.php
@@ -4,17 +4,20 @@
         <ul class="nav flex-column">
             @foreach($translations as $page => $trans)
                 <li class="nav-item">
-                    <a href="#" class="nav-link js-open-page-translation" data-target="content-page{{ str_slug($page) }}">{{ ucfirst($page) }}</a>
+                    <a href="#" class="nav-link js-open-page-translation" data-target="content-page-{{ str_slug($page) }}">{{ ucfirst($page) }}</a>
                 </li>
             @endforeach
         </ul>
     </div>
-    <div class="col-md-11">
+    <div class="col-md-10" style="background-color: rgb(240,240,240)">
         @foreach($translations as $page => $trans)
 
             <div class="content-page-translation" id="content-page-{{ str_slug($page) }}" style="display: none;">
                 @foreach($trans as $k=>$t)
-                    {{ var_dump($k,$t) }}
+                    <div class="form-group">
+                        <label for="trans-{{ str_slug($page) }}-{{ str_slug($k) }}" class="label-control">{{ $page.'.'.$k }}</label>
+                        <input type="text" id="trans-{{ str_slug($page) }}-{{ str_slug($k) }}" class="form-control" name="trans[{{ $page.'.'.$k }}" value="{{ $t }}" />
+                    </div>
                 @endforeach
             </div>
 
