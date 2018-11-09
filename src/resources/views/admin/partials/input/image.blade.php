d3d62065f2a20683cbc889172ad6300cc2fb1b2f
Jérémy Dillenbourg <jeremy.dillenbourg@bbdo.bne>
2018-11-09 16:32:11 +0100

    fix

--- src/resources/views/admin/partials/input/image.blade.php
+++ src/resources/views/admin/partials/input/image.blade.php
@@ -9,7 +9,7 @@
                 {{ $title }}
         </label>
         @endif
-        <div class="input-group image-preview" data-content="{{ $has_value ? $model->file_content($content[$type],$type) : '' }}" data-toggle="{{ $has_value ? 'popover' : '' }}">
+        <div class="input-group image-preview" data-content="{{ $has_value ? $model->fileContent($content[$type],$type) : '' }}" data-toggle="{{ $has_value ? 'popover' : '' }}">
           <div class="input-group-prepend" style="{{ $has_value ? "" : "display:none;" }}">
             <span class="input-group-text input-group-text-alt image-preview-clear"><i class="fa fa-euro-sign">clear</i></span>
           </div>
