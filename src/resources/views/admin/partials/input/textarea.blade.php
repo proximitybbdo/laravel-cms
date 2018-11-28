00e4d8ee7ffdd659c31baa3a10d54595f1dcb076
Jérémy Dillenbourg <jeremy.dillenbourg@bbdo.bne>
2018-11-28 10:14:50 +0100

    adding textarea type

---
+++ src/resources/views/admin/partials/input/textarea.blade.php
@@ -0,0 +1,12 @@
+<div class="form-group">
+    <label for="{{$field}}">
+        {{ $title }}
+    </label>
+
+    @if (isset($editor))
+        <?= Form::textarea($field, null, array('class' => 'form-control ' . $editor, 'id' => $field )); ?>
+    @else
+        <?= Form::textarea($field, null, array('class' => 'form-control', 'id' => $field, 'rows' => 10)); ?>
+    @endif
+
+</div>
