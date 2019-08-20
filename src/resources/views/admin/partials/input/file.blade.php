c5e5efc79e59689a00956aa004493cb6aadd200b
Jonathan Pardon <jonathan.pardon@bbdo.be>
2019-08-20 17:15:25 +0200

    temp file input layout fix

--- src/resources/views/admin/partials/input/file.blade.php
+++ src/resources/views/admin/partials/input/file.blade.php
@@ -1,24 +1,31 @@
-<div class="row {{ "cnt_" . $asset_field_name }}" style="display:{{ $multiple_fields && (!$has_value && $multiple_index > 0) ? 'none' : '' }}">
-  <div class="col-xs-12 col-md-6 col-sm-8">  
+<div class="form-group {{ "cnt_" . $asset_field_name }}" 
+  data-index="{{ $field_index }}"
+  style="display:{{ $multiple_fields && (!$has_value && $multiple_index > 0) ? 'none' : '' }}">
+
     @if(!$multiple_fields)
     <label class="control-label col-sm-12" for="{{ $field }}">
       {{ $title }}:
     </label>
     @endif
     <div class="input-group">
-      <?= Form::hidden($field, null, array('class' => 'form-control','id'=>$asset_field_name)); ?>
-      <?= Form::text('', ($has_value ? $model->file($content[$type])->file : ''), array('class' => 'form-control image-preview-filename','id'=>$asset_field_name,'disabled'=>'disabled')); ?>
-      <span class="input-group-btn">          
-          <button type="button" class="btn btn-default image-preview-clear" style="{{ $has_value ? "" : "display:none;" }}" data-input="<?= $asset_field_name ?>">
-              <span class="fa fa-times"></span>
-          </button> 
-          <div class="btn btn-default">              
-              <a class="showmanager" data-manager-type="file" data-module="<?= $module_type ?>" data-input="<?= $asset_field_name ?>"><span class="fa fa-folder-open-o"></span></a>             
-          </div>
-          <div class="btn btn-default" style="display:<?= $has_value ? "" : "none" ?>">
-              <a style="display:<?= $has_value ? "inline" : "none" ?>"  href="<?= url('/uploads/file/' . ($has_value ? $model->file($content[$type])->file : '')) ?>" id="file_content_<?= $type ?>"> <span class="fa fa-download"></span></a>            
-          </div>
-      </span>
+      <div class="input-group-prepend" style="{{ $has_value ? "" : "display:none;" }}">
+        <span class="input-group-text input-group-text-alt image-preview-clear" data-input="<?= $asset_field_name ?>"><i class="fa fa-unlink"></i></span>
+      </div>
+      <div class="custom-file">
+        <input type="text" class="form-control form-control-alt custom-file-input image-preview-filename" value="{{ $has_value ? $model->file($content[$type])->file : '' }}" disabled="disabled">
+      </div>
+      <div class="input-group-append">
+        <span class="input-group-text input-group-text-alt image-preview-input-title showmanager"
+            data-manager-type="image"
+            data-type="<?=$input_type?>"
+            data-module="<?=$module_type?>"
+            data-input="<?=$asset_field_name?>">
+          <i class="fa fa-file"></i>
+          <!-- <button c>Choose file</button> -->
+        </span>
+
+      </div>
     </div>
-  </div>
+
+  <?= Form::hidden($field, null, array('class' => 'form-control','id'=>$asset_field_name)); ?>
 </div>
